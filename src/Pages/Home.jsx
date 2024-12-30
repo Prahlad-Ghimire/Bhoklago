24934826f0bc42cc5e47f513a208d22c4cd31706
Prahlad-Ghimire <parhladghimire4545@gmail.com>
2024-12-31 00:05:32 +0545

    homesecondcontainer

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,16 +1,50 @@
 import React from "react";
+import { useState } from "react";
 import '../styles/Bhoklago.css';
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay } from "swiper/modules";
 import "swiper/css"
 function Home() {
+    const [content, setContent] = useState({
+        heading: "Quality Ingredients",
+        image: "https://img.freepik.com/free-photo/noodle-with-chicken-bowl-side-view_141793-2406.jpg?t=st=1735579906~exp=1735583506~hmac=69f5d5a522d5eeb1e58c7e02c62f57341c39c7d072450544526e00aca05f09b4&w=360",
+        l1: "Smooth and creamy texture, packed with healthy fats.",
+        l2: "Crisp and refreshing for that perfect crunch.",
+        l3: "Made with premium-quality oil for a rich, flavorful dressing.",
+    });
+
+    const data = {
+        mission: {
+            heading: "Quality Ingredients",
+            image: "https://img.freepik.com/free-photo/noodle-with-chicken-bowl-side-view_141793-2406.jpg?t=st=1735579906~exp=1735583506~hmac=69f5d5a522d5eeb1e58c7e02c62f57341c39c7d072450544526e00aca05f09b4&w=360", 
+            l1: "Smooth and creamy texture, packed with healthy fats.",
+            l2: "Crisp and refreshing for that perfect crunch.",
+            l3: "Made with premium-quality oil for a rich, flavorful dressing.",
+        },
+        vision: {
+            heading: "Quality Ingredients",
+            image: "https://img.freepik.com/free-photo/noodle-with-chicken-bowl-side-view_141793-2406.jpg?t=st=1735579906~exp=1735583506~hmac=69f5d5a522d5eeb1e58c7e02c62f57341c39c7d072450544526e00aca05f09b4&w=360",
+            l1: "Fragrant and aromatic, bringing out the best in every dish.",
+            l2: "Aged to perfection for added depth and richness.",
+            l3: "Elevates the natural flavors of your food with every bite.",
+        },
+        value: {
+            heading: "Quality Ingredients",
+            image: "https://img.freepik.com/free-photo/noodle-with-chicken-bowl-side-view_141793-2406.jpg?t=st=1735579906~exp=1735583506~hmac=69f5d5a522d5eeb1e58c7e02c62f57341c39c7d072450544526e00aca05f09b4&w=360",
+            l1: "Delightfully sweet with a touch of caramel undertones.",
+            l2: "Rich and aromatic, amplifying the overall flavor.",
+            l3: "Made with premium-quality eggs for perfect moisture and structure.",
+        },
+    };
+
+    const handleClick = (type) => {
+        setContent(data[type]);
+    };
     return (
         <div>
-            <div className="container-fluid px-0">
+            <div className="container-fluid px-0 pb-5">
                 <Swiper
-
                     loop={true}
-
                     autoplay={{
                         delay: 1500,
                         disableOnInteraction: false,
@@ -44,7 +78,7 @@ function Home() {
                         </div>
                     </SwiperSlide>
                     <SwiperSlide>
-                    <div className="position-relative">
+                        <div className="position-relative">
                             <div className="">
                                 <img src="https://img.freepik.com/free-photo/top-view-delicious-food-with-copy-space_23-2150873986.jpg?t=st=1735574159~exp=1735577759~hmac=23a787dd0cfd22f43642b0253a58ae47faf5e95df5a0b3368947091d75f651a8&w=1060" className="w-100 object-fit-cover" style={{ height: 500 }} />
                             </div>
@@ -58,9 +92,42 @@ function Home() {
                         </div>
                     </SwiperSlide>
                 </Swiper>
+            </div>
+            <div className="container mt-5 mb-5">
                 <div className="row">
-                    <div className="col-lg-12">
-
+                    <div className="col-lg-6">
+                        <div className="fresh position-relative w-100">
+                            <img src="https://img.freepik.com/free-photo/crispy-mix-pizza-with-sausage_140725-2281.jpg?t=st=1735575595~exp=1735579195~hmac=453b230443e512e31c00c3a45b0b45f81938c02dcba573e6b7f9d142614564f8&w=740" className="z-2 shadow position-relative rounded-3 pe-0" alt="" style={{ height: 600 }} />
+                            <div className="position-absolute z-2 bg-light border-5 rounded-2 shadow" style={{ bottom: 55, right: -10 }}>
+                                <img src="https://img.freepik.com/free-photo/front-view-woman-with-delicious-pizza_23-2150347910.jpg?t=st=1735577558~exp=1735581158~hmac=b2ae2b1c0b0c8856883b73be06abc6a782fead568c458c0dd971d21dac67aa5b&w=996" alt="" className="p-2" style={{ width: 200, height: 200 }} />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="px-4">
+                            <h2 className="fs-1 text-capitalize">Fresh Home food <strong>delivered at your</strong> Door Step</h2>
+                            <p className="fs-5 mt-4" style={{ textAlign: "justify" }}>At Bhoklago, we make enjoying delicious, wholesome meals effortless. With just a few clicks, you can have home-style food delivered straight to your door, made from the freshest ingredients and authentic recipes.</p>
+                            <div className="grp-button d-flex justify-content-between">
+                                <button className="btn border-0 fw-bold fs-5" onClick={() => handleClick("mission")}>Our Mission</button>
+                                <button className="btn border-0 fw-bold fs-5" onClick={() => handleClick("vision")}>Our Vision</button>
+                                <button className="btn border-0 fw-bold fs-5" onClick={() => handleClick("value")}>Our Value</button>
+                            </div>
+                            <div className="row mt-5">
+                                <div className="col-lg-6">
+                                    <img src="{content.image}" alt="" />
+                                </div>
+                                <div className="col-lg-6">
+                                    <p className="px-3 fs-5 fw-bold">{content.heading}</p>
+                                    <div>
+                                        <ul className="text-capitalize">
+                                            <li className="fs-6">{content.l1}</li>
+                                            <li className="fs-6">{content.l2}</li>
+                                            <li className="fs-6">{content.l3}</li>
+                                        </ul>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
