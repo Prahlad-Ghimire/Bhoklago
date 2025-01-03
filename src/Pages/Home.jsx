85967373c918c919ec23a03fb0342e00b9d3593d
Prahlad-Ghimire <parhladghimire4545@gmail.com>
2025-01-03 23:37:21 +0545

    homeupdated

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -136,17 +136,37 @@ function Home() {
                     </div>
                 </div>
             </div>
-            <div className="container-fluid">
+            <div className="container-fluid" style={{ backgroundColor: '#656565' }}>
                 <div className="row">
-                    <div className="col-lg-12 px-0">
-                        <div className="position-relative">
-                            <img src="https://img.freepik.com/free-photo/top-view-cooked-tasty-squashes-with-seasonings-bread-loafs-dark-purple-desk_140725-50075.jpg?t=st=1735744069~exp=1735747669~hmac=977339546487e3db9de485d23bc573cde49c145f0a78603e5e4ccfcaab50b433&w=996" alt="" className="w-100" style={{height:700}} />
-                            <div className="position-absolute w-100 top-0 opacity-50">
-                                <img src="https://img.freepik.com/free-photo/abstract-textured-backgound_1258-30461.jpg?t=st=1735568824~exp=1735572424~hmac=36549b9354bccb90f01c459a9a09fe7fbea68844002ad804e391ede17dd2fac3&w=740" alt="" className="w-100" style={{height:700}} />
+                    <div className="col-lg-6">
+                        <div className="px-4 mb-5">
+                            <h1 className="text-capitalize text-white mt-5">Satisfy Your Cravings <br /> Anytime, Anywhere</h1>
+                            <div className="d-flex align-items-center gap-2 mt-4">
+                                <p className="mb-0 fs-4 fw-bold">Quick Response</p>
+                                <div className="rounded-circle px-2 py-1 fs-4 d-inline-block" style={{ backgroundColor: "#F8F9FA" }}>
+                                    <i class="bi bi-rocket-takeoff"></i>
+                                </div>
                             </div>
-                            <div className="position-absolute top-50 text-white">
-                                <p>At BhokLago, we specialize in delivering exceptional dining experiences by bringing meals from your favorite restaurants directly to your home, office, or any location of your choice.We recognize that time is a valuable resource, and every moment counts. That’s why we are committed to providing a fast, seamless, and reliable delivery service, allowing you to focus on what truly matters while we handle your culinary needs.</p>
+                            <p className="text-white px-4 mt-3" style={{textAlign:'justify'}}>We understand hunger can't wait! That's why we ensure fast and reliable communication to meet your needs instantly.</p>
+                            <div className="d-flex align-items-center gap-2 mt-4">
+                                <p className="mb-0 fs-4 fw-bold">Skilled Team</p>
+                                <div className="rounded-circle px-2 py-1 fs-4 d-inline-block" style={{ backgroundColor: "#F8F9FA" }}>
+                                <i class="bi bi-people-fill"></i>
+                                </div>
                             </div>
+                            <p className="text-white px-4 mt-3" style={{textAlign:'justify'}}>Our team of expert chefs and passionate food lovers work tirelessly to bring you fresh, delicious meals inspired by authentic family recipes.</p>
+                            <div className="d-flex align-items-center gap-2 mt-4">
+                                <p className="mb-0 fs-4 fw-bold">Customer First</p>
+                                <div className="rounded-circle px-2 py-1 fs-4 d-inline-block" style={{ backgroundColor: "#F8F9FA" }}>
+                                <i class="bi bi-person"></i>
+                                </div>
+                            </div>
+                            <p className="text-white px-4 mt-3" style={{textAlign:'justify'}}>Your satisfaction is our priority. We strive to deliver meals that bring the warmth of home to your doorstep, making every bite memorable.</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-6">
+                        <div className="h-100 d-flex align-items-center mt-0 mt-lg-3">
+                            <img src="https://img.freepik.com/free-photo/georgian-eggplant-roulettes-table_140725-9215.jpg?t=st=1735925307~exp=1735928907~hmac=b5ef6a0b5c70abd9603ae945aea74e8b9b2d34eb4b540e767534ce0d06923bb9&w=360" alt="" className="w-100 rounded-3 img-thumbnail mb-2" style={{height:450}}/>
                         </div>
                     </div>
                 </div>
