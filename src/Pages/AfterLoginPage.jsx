ba81ec26c325afdb827089e22f3f37c1ec6c4308
Prahlad-Ghimire <parhladghimire4545@gmail.com>
2025-01-16 22:12:19 +0545

    restauranttypes

--- src/Pages/AfterLoginPage.jsx
+++ src/Pages/AfterLoginPage.jsx
@@ -14,35 +14,92 @@ const AfterLoginPage = () => {
                                     <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Sandar Momo, your ultimate destination for authentic and delicious momo! Specializing in Chicken, Buff, and Veg Momos, we steam each one to perfection using fresh, high-quality ingredients to ensure a burst of flavor in every bite. Prepared with love and attention to detail, our momos deliver a wholesome and satisfying experience that keeps customers coming back for more. Whether you're a devoted momo fan or trying them for the first time, Sandar Momo is here to delight your taste buds!</p>
                                 </div>
                                 <div className="frontface position-absolute top-0 w-100 d-flex justify-content-center align-items-center">
-                                    <img src="https://english.onlinekhabar.com/wp-content/uploads/2021/01/New-Everest.jpg" alt="" className='w-100 h-100' />
+                                    <img src="https://img.freepik.com/free-photo/gyoza-with-soy-sauce-chopsticks-plate_141793-1722.jpg?t=st=1737040044~exp=1737043644~hmac=de0032f98402c006cb0dcb1b69b806bb9e60cc00ebea1d21c0fa29e5678c0424&w=360" alt="" className='w-100 h-100' />
                                 </div>
                             </Link>
+                        </div>
+                        <div className='d-flex justify-content-center mt-3 w-100'>
+                            <p className='text-uppercase fw-bold fs-5 mb-0'>Sandar Momo, New-Baneshwor</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className='w-100 d-flex justify-content-center mt-5'>
                             <Link to="" className="restaurant position-relative d-flex justify-content-center align-items-center">
                                 <div className='details position-absolute px-4'>
-                                    <p className='text-white restauranthead fw-bold text-uppercase'>Sandar Momo</p>
-                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Sandar Momo, your ultimate destination for authentic and delicious momo! Specializing in Chicken, Buff, and Veg Momos, we steam each one to perfection using fresh, high-quality ingredients to ensure a burst of flavor in every bite. Prepared with love and attention to detail, our momos deliver a wholesome and satisfying experience that keeps customers coming back for more. Whether you're a devoted momo fan or trying them for the first time, Sandar Momo is here to delight your taste buds!</p>
+                                    <p className='text-white restauranthead fw-bold text-uppercase'>Bricks Cafe</p>
+                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Bricks Cafe, your ultimate destination for delicious pizzas and wholesome, healthy meals! Specializing in a variety of freshly made pizzas and an array of nutritious dishes, we use only the finest ingredients to ensure exceptional taste and quality in every bite. Prepared with care and attention to detail, our food delivers a satisfying and flavorful experience that keeps customers coming back for more. Whether you’re a pizza lover or looking for a hearty, balanced meal, Bricks Cafe is here to exceed your expectations!</p>
                                 </div>
                                 <div className="frontface position-absolute top-0 w-100 d-flex justify-content-center align-items-center">
-                                    <img src="https://english.onlinekhabar.com/wp-content/uploads/2021/01/New-Everest.jpg" alt="" className='w-100 h-100' />
+                                    <img src="https://img.freepik.com/free-photo/side-view-pizza-with-slices-bell-pepper-pizza-slices-flour-board-cookware_176474-3185.jpg?t=st=1737037294~exp=1737040894~hmac=a303abd256571cb142f97327b3a183cdd147d47909799b41af44887ba152c390&w=740" alt="" className='w-100 h-100' />
                                 </div>
                             </Link>
+                        </div>
+                        <div className='d-flex justify-content-center mt-3 w-100'>
+                            <p className='text-uppercase fw-bold fs-5 mb-0'>Bricks Cafe, Thamel</p>
                         </div>
                     </div>
                     <div className="col-lg-4">
                         <div className='w-100 d-flex justify-content-center mt-5'>
                             <Link to="" className="restaurant position-relative d-flex justify-content-center align-items-center">
                                 <div className='details position-absolute px-4'>
-                                    <p className='text-white restauranthead fw-bold text-uppercase'>Sandar Momo</p>
-                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Sandar Momo, your ultimate destination for authentic and delicious momo! Specializing in Chicken, Buff, and Veg Momos, we steam each one to perfection using fresh, high-quality ingredients to ensure a burst of flavor in every bite. Prepared with love and attention to detail, our momos deliver a wholesome and satisfying experience that keeps customers coming back for more. Whether you're a devoted momo fan or trying them for the first time, Sandar Momo is here to delight your taste buds!</p>
+                                    <p className='text-white restauranthead fw-bold text-uppercase'>Mustang Thakali</p>
+                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Mustang Thakali, your ultimate destination for authentic Thakali cuisine and wholesome, hearty meals! Specializing in a variety of traditional Thakali dishes and other nutritious options, we use only the finest ingredients to ensure exceptional taste and quality in every bite. Prepared with care and attention to detail, our food delivers a satisfying and flavorful experience that keeps customers coming back for more. Whether you’re craving Thakali flavors or looking for a wholesome meal, Mustang Thakali is here to exceed your expectations!</p>
                                 </div>
                                 <div className="frontface position-absolute top-0 w-100 d-flex justify-content-center align-items-center">
-                                    <img src="https://english.onlinekhabar.com/wp-content/uploads/2021/01/New-Everest.jpg" alt="" className='w-100 h-100' />
+                                    <img src="https://img.freepik.com/free-photo/close-up-appetizing-ramadan-meal_23-2151182518.jpg?t=st=1737038651~exp=1737042251~hmac=741d2f9257cec8a6084a8b7105f29ace6570244eb49d2aa6d713fbc31e24260c&w=996" alt="" className='w-100 h-100' />
                                 </div>
                             </Link>
+                        </div>
+                        <div className='d-flex justify-content-center mt-3 w-100'>
+                            <p className='text-uppercase fw-bold fs-5 mb-0'>Mustang Thakali, Tinkune</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className='w-100 d-flex justify-content-center mt-5'>
+                            <Link to="" className="restaurant position-relative d-flex justify-content-center align-items-center">
+                                <div className='details position-absolute px-4'>
+                                    <p className='text-white restauranthead fw-bold text-uppercase'>Chicken Hub</p>
+                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Chicken Hub, your ultimate destination for a wide variety of delicious food and snacks! From juicy momos and flavorful chowmein to aromatic biryani and crispy fried chicken, we’ve got something for everyone. Pair your meal with refreshing beverages like Coke or enjoy a chilled beer to elevate your dining experience. At Chicken Hub, we take pride in serving high-quality, freshly prepared dishes that satisfy every craving. Whether you’re here for a quick snack or a hearty meal, Chicken Hub is the perfect spot to indulge in great food and good vibes!</p>
+                                </div>
+                                <div className="frontface position-absolute top-0 w-100 d-flex justify-content-center align-items-center">
+                                    <img src="https://img.freepik.com/free-photo/view-delicious-food-chinese-new-year-reunion-dinner_23-2151040692.jpg?t=st=1737039232~exp=1737042832~hmac=3fe4060010d3062d6b012824490134d30c8e0e8f46697bc68879d83bff1a7722&w=740" alt="" className='w-100 h-100' />
+                                </div>
+                            </Link>
+                        </div>
+                        <div className='d-flex justify-content-center mt-3 w-100'>
+                            <p className='text-uppercase fw-bold fs-5 mb-0'>Chicken hub, Balkot</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className='w-100 d-flex justify-content-center mt-5'>
+                            <Link to="" className="restaurant position-relative d-flex justify-content-center align-items-center">
+                                <div className='details position-absolute px-4'>
+                                    <p className='text-white restauranthead fw-bold text-uppercase'>Kruchy Fried Chicken (kfc)</p>
+                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to KFC, the home of the world’s most iconic fried chicken! Famous for our signature blend of 11 herbs and spices, we serve crispy, juicy, and flavorful chicken that's loved by millions. From our classic fried chicken and zinger burgers to wings, fries, and refreshing beverages, there’s something for everyone to enjoy. Whether you’re grabbing a quick bite or sharing a bucket with friends and family, KFC delivers a delicious and satisfying experience every time. Great food, great taste—only at KFC!</p>
+                                </div>
+                                <div className="frontface position-absolute top-0 w-100 d-flex justify-content-center align-items-center">
+                                    <img src="https://img.freepik.com/free-photo/vertical-shot-delicious-crispy-fried-chicken-ketchup-gravy-celery-sticks-wooden-table_181624-56089.jpg?t=st=1737040765~exp=1737044365~hmac=5aaa1227f993a737e6e10e9430dacc275d72c95a7eefd2a30c72c7da68bd5d87&w=360" alt="" className='w-100 h-100' />
+                                </div>
+                            </Link>
+                        </div>
+                        <div className='d-flex justify-content-center mt-3 w-100'>
+                            <p className='text-uppercase fw-bold fs-5 mb-0'>KFC, Radhe Radhe</p>
+                        </div>
+                    </div>
+                    <div className="col-lg-4">
+                        <div className='w-100 d-flex justify-content-center mt-5'>
+                            <Link to="" className="restaurant position-relative d-flex justify-content-center align-items-center">
+                                <div className='details position-absolute px-4'>
+                                    <p className='text-white restauranthead fw-bold text-uppercase'>Bhakka House</p>
+                                    <p className='text-white restaurantdetail text-capitalize' style={{textAlign:'justify'}}>Welcome to Bhakka House, your ultimate destination for authentic Nepali flavors! Known for our signature steamed rice cakes, Bhakka, we bring you a taste of tradition paired with delicious side dishes like spicy chutneys and hearty snacks. Prepared with love and fresh ingredients, our food offers a wholesome and flavorful experience. Whether you’re craving a light bite or a comforting meal, Bhakka House is here to satisfy your taste buds and celebrate Nepal’s rich culinary heritage!</p>
+                                </div>
+                                <div className="frontface position-absolute top-0 w-100 d-flex justify-content-center align-items-center">
+                                    <img src="https://english.onlinekhabar.com/wp-content/uploads/2018/12/Bhakka-1-1.jpg" alt="" className='w-100 h-100' />
+                                </div>
+                            </Link>
+                        </div>
+                        <div className='d-flex justify-content-center mt-3 w-100'>
+                            <p className='text-uppercase fw-bold fs-5 mb-0'>Bhakka House, Mid-Baneshwor</p>
                         </div>
                     </div>
                 </div>
